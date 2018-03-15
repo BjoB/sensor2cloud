baac721da0a6f0c831b8ac63d8146846b39a9bc4
Bat <bjoern.barschtipan@gmail.com>
2018-03-15 23:21:45 +0100

    update view by wrapping model update in ngZone

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
@@ -6,6 +6,7 @@ import { BLE } from '@ionic-native/ble';
 
 const SENSORTAG_TEMP_SERVICE = 'f000aa20-0451-4000-b000-000000000000';
 const SENSORTAG_TEMP_CHARACTERISTIC = 'f000aa21-0451-4000-b000-000000000000';
+const SENSORTAG_TEMP_CONF_CHARACTERISTIC = 'f000aa22-0451-4000-b000-000000000000';
 const SENSORTAG_DEVICE_NAME = 'CC2650 SensorTag';
 
 
@@ -24,7 +25,8 @@ export class HomePage {
   devices = [];
   statusMessage: string;
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
+              private ngZone: NgZone, 
               private toastCtrl: ToastController, 
               private ble: BLE) {
 
@@ -85,24 +87,41 @@ export class HomePage {
     
     if (this.devices.find(device => device.id == peripheralData.id) == undefined) {
       //TODO: Index anhängen, da mehrere Sensortags vorhanden sein können -> 'uniqueName' als property setzen
-      this.devices.push(deviceData);
 
-      this.ble.startNotification(peripheralData.id, SENSORTAG_TEMP_SERVICE, SENSORTAG_TEMP_CHARACTERISTIC).subscribe(
-        buffer => {
-          var sensorData = new Uint16Array(buffer);
-          var rawTemp = sensorData[0];
-          var rawHum = sensorData[1];
-          var temp = (rawTemp / 65536)*165 - 40;
-          //rawHum &= ~0x0003;
-          var hum = (rawHum / 65536)*100;
+      this.ngZone.run(() => {
+        this.devices.push(deviceData);
+      });
 
-          deviceData.temperature = temp;
-          deviceData.humidity = temp;
-          console.log('Temperature: ' + temp.toString + '°C');
-        },
-        () => console.log('Unexpected Error: Failed to subscribe for temperature changes.')
+      //activate notifications
+      var data = new Uint8Array(1);
+      data[0] = 1;
+      this.ble.write(deviceData.id, SENSORTAG_TEMP_SERVICE, SENSORTAG_TEMP_CONF_CHARACTERISTIC, data.buffer)
+      .then(
+        () => this.onInitialConfigFinished(deviceData)
+      ).catch(
+        err => console.log('Error on writing to config characteristic(' + err.toString() + ')')
       );
     }
+  }
+
+  onInitialConfigFinished(deviceData) {
+    this.ble.startNotification(deviceData.id, SENSORTAG_TEMP_SERVICE, SENSORTAG_TEMP_CHARACTERISTIC).subscribe(
+      buffer => {
+        var sensorData = new Uint16Array(buffer);
+        var rawTemp = sensorData[0];
+        var rawHum = sensorData[1];
+        var temp = (rawTemp / 65536)*165 - 40;
+        //rawHum &= ~0x0003;
+        var hum = (rawHum / 65536)*100;
+        
+        this.ngZone.run(() => {
+          deviceData.temperature = temp.toFixed(2);
+          deviceData.humidity = hum.toFixed(2);
+        });
+        //console.log('Temp.: ' + deviceData.temperature + ' °C -- Hum.: ' + deviceData.humidity + ' %');
+      },
+      () => console.log('Unexpected Error: Failed to subscribe for temperature changes.')
+    );
   }
 
   onScanError(error) {
